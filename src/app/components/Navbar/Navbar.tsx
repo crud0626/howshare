0ebf18a6840f58c81e24802ab0e9c4a4468c4139
crud0626 <sisofiy6048@gmail.com>
2023-12-22 14:33:47 +0900

    Feat: 카테고리 영역 작업
    * query-string 패키지 추가

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Container from "../Container"
 import Logo from "./Logo"
 import SearchBar from "./SearchBar"
 import UserMenu from "./UserMenu"
+import Categories from "./Categories"
 
 interface NavbarProps {
   currentUser?: User | null
@@ -22,6 +23,7 @@ const Navbar = ({ currentUser }: NavbarProps) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
