0887234858826f6fa0a60e90f88ffe9227e9ee8a
crud0626 <sisofiy6048@gmail.com>
2023-12-23 00:35:07 +0900

    Feat: 숙소디테일 페이지 작업

---
+++ src/app/listings/[listingId]/page.tsx
@@ -0,0 +1,25 @@
+import EmptyState from "@/app/EmptyState"
+import getCurrentUser from "@/app/actions/getCurrentUser"
+import getListingById from "@/app/actions/getListingById"
+import ListingClient from "./ListingClient"
+
+interface IParams {
+  listingId?: string
+}
+
+const ListingDetailPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById(params)
+  const currentUser = await getCurrentUser()
+
+  if (!listing) {
+    return <EmptyState />
+  }
+
+  return (
+    <div>
+      <ListingClient listing={listing} currentUser={currentUser} />
+    </div>
+  )
+}
+
+export default ListingDetailPage
