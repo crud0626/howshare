35bb7df87c028689b62eefe254b9aef8d06c1a83
crud0626 <sisofiy6048@gmail.com>
2023-12-25 23:11:26 +0900

    Feat: 서치바 영역 조건부 텍스트 추가

--- src/app/components/Navbar/SearchBar.tsx
+++ src/app/components/Navbar/SearchBar.tsx
@@ -1,10 +1,50 @@
 "use client"
 
 import { BiSearch } from "react-icons/bi"
+import { useSearchParams } from "next/navigation"
+
 import useSearchModal from "@/app/hooks/useSearchModal"
+import useCountries from "@/app/hooks/useContries"
+import { useMemo } from "react"
+import { differenceInDays } from "date-fns"
 
 const SearchBar = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCountries()
+
+  const locationValue = params?.get("locationValue")
+  const startDate = params?.get("startDate")
+  const endDate = params?.get("endDate")
+  const guestCount = params?.get("guestCount")
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue)?.label
+    }
+
+    return "어디든지"
+  }, [getByValue, locationValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate)
+      const end = new Date(endDate)
+      let diff = differenceInDays(end, start)
+
+      if (diff === 0) diff = 1
+
+      return `${diff} 일`
+    }
+
+    return "언제든지"
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) return `${guestCount} 명`
+
+    return "인원 추가"
+  }, [guestCount])
 
   return (
     <div
@@ -12,10 +52,12 @@ const SearchBar = () => {
       className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
     >
       <div className="flex flex-row items-center justify-between">
-        <div className="text-sm font-semibold px-6">Any</div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">Any Week</div>
+        <div className="text-sm font-semibold px-6">{locationLabel}</div>
+        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+          {durationLabel}
+        </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <BiSearch size={18} />
           </div>
