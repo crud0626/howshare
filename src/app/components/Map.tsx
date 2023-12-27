fcce266caf554f3fa0364298ad97cbde07db7c82
crud0626 <sisofiy6048@gmail.com>
2023-12-27 10:02:11 +0900

    Feat: 카카오 맵 적용
    * 좌표와 주소 변환을 위한 geocoder lib 파일 추가

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,38 +1,41 @@
 "use client"
 
-import L from "leaflet"
-import { MapContainer, Marker, TileLayer } from "react-leaflet"
-
-import "leaflet/dist/leaflet.css"
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
-import markerIcon from "leaflet/dist/images/marker-icon.png"
-import markerShadow from "leaflet/dist/images/marker-shadow.png"
-
-// @ts-ignore
-delete L.Icon.Default.prototype._getIconUrl
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon.src,
-  iconRetinaUrl: markerIcon2x.src,
-  shadowUrl: markerShadow.src,
-})
+import { useEffect, useState } from "react"
+import { Map as MapContainer, MapMarker, _MapProps } from "react-kakao-maps-sdk"
+import { addressToCoord } from "../lib/geocoder"
 
 interface MapProps {
   center?: number[]
+  userAddress?: string
+  onChange?: (addr: string) => void
 }
 
-const Map = ({ center }: MapProps) => {
+const initialCenter = {
+  lat: 37.5666,
+  lng: 126.9782,
+}
+
+const Map = ({ center: centerProps, userAddress }: MapProps) => {
+  const [center, setCenter] = useState(
+    centerProps
+      ? {
+          lat: centerProps[0],
+          lng: centerProps[1],
+        }
+      : initialCenter,
+  )
+
+  useEffect(() => {
+    if (userAddress) {
+      addressToCoord(userAddress, coordi => {
+        setCenter(coordi)
+      })
+    }
+  }, [userAddress])
+
   return (
-    <MapContainer
-      center={(center as L.LatLngExpression) || [51, -0.09]}
-      zoom={center ? 4 : 2}
-      scrollWheelZoom={false}
-      className="h-[35vh] rounded-lg"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {center && <Marker position={center as L.LatLngExpression} />}
+    <MapContainer style={{ height: "35vh", borderRadius: "0.5rem" }} center={center}>
+      <MapMarker position={center} />
     </MapContainer>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Nunito } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 
 import getCurrentUser from "./actions/getCurrentUser"
@@ -23,6 +24,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang="ko">
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
+        strategy="afterInteractive"
+      />
       <body className={font.className}>
         <RegisterModal />
         <LoginModal />
