5fe8576318d43794824be4b4c623bbd95576509e
crud0626 <sisofiy6048@gmail.com>
2023-12-27 21:54:50 +0900

    Feat: 폰트 파일 추가 및 로컬 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Nunito } from "next/font/google"
+import localFont from "next/font/local"
 import Script from "next/script"
 import "./globals.css"
 
@@ -12,7 +12,26 @@ import SearchModal from "./components/modals/SearchModal"
 import RentModal from "./components/modals/RentModal"
 import ToastProvider from "./components/ToastProvider"
 
-const font = Nunito({ subsets: ["latin"] })
+const pretendard = localFont({
+  src: [
+    {
+      path: "./fonts/Pretendard-Bold.woff2",
+      weight: "900",
+    },
+    {
+      path: "./fonts/Pretendard-SemiBold.woff2",
+      weight: "700",
+    },
+    {
+      path: "./fonts/Pretendard-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "./fonts/Pretendard-Regular.woff2",
+      weight: "400",
+    },
+  ],
+})
 
 export const metadata: Metadata = {
   title: "HowShare",
@@ -29,7 +48,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
         strategy="afterInteractive"
       />
-      <body className={font.className}>
+      <body className={pretendard.className}>
         <RegisterModal />
         <LoginModal />
         <RentModal />
