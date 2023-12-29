e4caeafce8e610955733cb4668a447f09e747102
crud0626 <sisofiy6048@gmail.com>
2023-12-30 00:47:54 +0900

    Style: ListingCard 스타일 변경
    * 카드의 텍스트 및 스타일 변경
    
    * 1줄 처리되도록 스타일 추가

--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -72,7 +72,10 @@ const ListingCard = ({
           </div>
         </div>
         <div onClick={() => router.push(`/listings/${data.id}`)} className="p-3 gap-2 flex flex-col">
-          <div className="font-semibold text-lg">{data.locationValue}</div>
+          <div>
+            <div className="font-semibold text-lg truncate">{data.title}</div>
+            <div className="font-light text-neutral-500 truncate">{data.locationValue}</div>
+          </div>
           <div className="font-light text-neutral-500">{reservationDate || data.category}</div>
           <div className="flex flex-row items-center gap-1">
             <div className="font-semibold">￦ {price.toLocaleString()}</div>
